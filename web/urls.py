48a7ea1dc18472bd4d1d50d119a531d55d4dcd4b
Bandaggao <banadraind@gmail.com>
2018-09-21 09:11:47 +0800

    Merge Branches

--- web/urls.py
+++ web/urls.py
@@ -3,10 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='web-home'),
-<<<<<<< HEAD
     path('login/', views.login, name='web-login'),
-=======
     path('department_list/', views.departmentlist, name='web-dep'),
     path('rating_page/', views.ratingpage, name='web-rp'),
->>>>>>> initial-rating-page-views
+
 ]
--- web/views.py
+++ web/views.py
@@ -1,19 +1,16 @@
 from django.shortcuts import render
 
-<<<<<<< HEAD
+
 def home(request):
     return render(request, 'web/home.html')
 
-<<<<<<< HEAD
 def login(request):
     return render(request, 'web/login.html')
-=======
+
 def departmentlist(request):
     return render(request, 'web/department_list.html')
 
 def ratingpage(request):
     return render(request, 'web/rating_page.html')
->>>>>>> initial-rating-page-views
-=======
-# Create your views here.
->>>>>>> Initial-Model
+
+
